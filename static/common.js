e254296579fe813bd291f8be63d0e34b9cdfd975
Jinesh <jineshpaloor@gmail.com>
2015-09-29 10:17:23 +0530

    per document topic visualization added

--- static/common.js
+++ static/common.js
@@ -39,7 +39,6 @@ NS.topics = (function () {
     function register_topic_pie_chart() {
         $(".topic").hover(function(){
             var topic_id = $(this).attr("id"); 
-            console.log(topic_id);
             render_pie_chart(topic_id);
         });
     }
@@ -59,5 +58,64 @@ NS.topics = (function () {
         init: init
     };
 
-/** This will make it possible to override config before calling implementation */
+})();
+
+NS.documents_topics = (function () {
+    // define some config (Settings)
+    var config = {
+        autoInvokeInit: false, // whether the init function must be invoked automatically when page loads
+        files: {},
+        doc_topics: {}
+    };
+
+    function update_docs_dropdown_menu() {
+        var li_menu = "";
+        $.each(config.doc_topics, function(k, v){
+            li_menu += "<li class='wiki-document'><a href='#'>" + k + "</a></li>";
+        });
+        $("#documents-dropdown-menu").html(li_menu);
+        register_dropdown_change();
+    };
+
+    function render_doc_pie_chart(doc_name) {
+        var chart = c3.generate({
+            data: {
+                // iris data from R
+                columns: config.doc_topics[doc_name],
+                type : 'pie',
+                onclick: function (d, i) { 
+                    console.log("topic is :", d["id"]); 
+                    // NS.topics.render_pie_chart("topic" + d["id"])
+                },
+                //onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+                //onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+            }
+        });
+        $("#chart-title").text(doc_name);
+    }
+
+    function register_dropdown_change() {
+        $(".wiki-document").on("click", function(){
+            var doc_name = $(this).text();
+            render_doc_pie_chart(doc_name);
+        });
+    };
+
+    // define the init function (Implementation)
+    var init = function () {
+        $.getJSON("wiki_files.json", function(file_names) {
+            config.files = file_names; 
+        });
+        $.getJSON("doc_topics.json", function(doc_topics) {
+            config.doc_topics = doc_topics; 
+            update_docs_dropdown_menu();
+            render_doc_pie_chart(config.files[0]);
+        });
+    };
+    
+    // return an object
+    return {
+        config: config,
+        init: init
+    };
 })();
